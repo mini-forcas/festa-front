e536111ed6f62ca4680aa1a13d8fb301f95e103f
Kazutake.Yamamoto <kazutake.yamamoto@uzabase.com>
2023-07-13 19:27:04 +0900

    push

--- app/festa/page.tsx
+++ app/festa/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import React from "react";
+import { io } from "socket.io-client";
 
 function page() {
+  const host = "http://localhost";
+  const port = 5000;
+  const socket = io(`${host}:${port}`);
+  socket.on("connection", () => console.log("connect"));
+  // send message to server
+  function submitMessage() {
+    let counter = 0;
+    setInterval(() => {
+      socket.emit("yyy", { message: `client message ${counter++}` });
+    });
+  }
   return (
     <div>
       {/* TODO クリックすると管理者に選択した答えが送信される */}
-      <button className="btn">1</button>
+      <button className="btn" onClick={() => submitMessage()}>
+        1
+      </button>
       <button className="btn">2</button>
       <button className="btn">3</button>
       <button className="btn">4</button>
