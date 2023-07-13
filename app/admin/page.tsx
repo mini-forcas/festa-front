bf54288aa9d3b656e9ff51032f8ad2fdad49c1f7
Kazutake.Yamamoto <kazutake.yamamoto@uzabase.com>
2023-07-13 18:54:54 +0900

    test front

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      {/* TODO これをクリックするとクライアントに回答が表示される */}
+      <button className="btn bg-teal-300">回答</button>
+    </div>
+  );
+}
---
+++ app/festa/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      {/* TODO クリックすると管理者に選択した答えが送信される */}
+      <button className="btn">1</button>
+      <button className="btn">2</button>
+      <button className="btn">3</button>
+      <button className="btn">4</button>
+    </div>
+  );
+}
+
+export default page;
