32fd462205a39ac621a8772a7dc8a0dbaaec4da9
mixnuts <french0719@gmail.com>
2023-08-03 18:26:44 +0900

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <button className="btn">
+        <Link href="/">HOME</Link>
+      </button>
+      <button className="btn">
+        <Link href="/admin">ADMIN</Link>
+      </button>
+    </div>
+  );
+};
