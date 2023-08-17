b2fb1528da2b780f9131809201b0e87fc6a5b582
mixnuts <french0719@gmail.com>
2023-08-17 19:26:17 +0900

    add question

--- app/festa/page.tsx
+++ app/festa/page.tsx
@@ -1,6 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import { createClient } from "@supabase/supabase-js";
+
+type DataType = {
+  id: number;
+  admin_id: number;
+  question: string;
+  correct_option_id: number;
+  created_at: string;
+  updated_at: string;
+};
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+);
 
 function page() {
   const host = "http://localhost";
@@ -8,6 +22,7 @@ function page() {
   const socket = io(`${host}:${port}`);
   const [answer, setAnswer] = useState("");
   const [myAnswer, setMyAnswer] = useState<number>();
+  const [data, setData] = useState<DataType[]>([]);
 
   socket.on("connection", () => console.log("connect"));
   socket.on("receiveAnswer", (ans) => {
@@ -19,8 +34,19 @@ function page() {
     socket.emit("submitAnswer", ans);
     setMyAnswer(ans);
   }
+  const fetch = async () => {
+    const { data, error } = await supabase.from("quizzes").select();
+    if (data === null) return;
+    setData(data);
+    console.log(data[0]);
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <div>
+      <div>{data[0]?.id}</div>
       {/* TODO クリックすると管理者に選択した答えが送信される */}
       <button className="btn" onClick={() => submitAnswer(1)}>
         1
