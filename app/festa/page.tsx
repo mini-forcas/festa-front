a1a82578c560429a5c8145d13b0e535fa5fc7e41
mixnuts <french0719@gmail.com>
2023-08-31 18:53:56 +0900

    add question

--- app/festa/page.tsx
+++ app/festa/page.tsx
@@ -11,6 +11,13 @@ type DataType = {
   created_at: string;
   updated_at: string;
 };
+type OptionsType = {
+  id: number;
+  quiz_id: number;
+  option_text: string;
+  created_at: string;
+  updated_at: string;
+};
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
@@ -21,8 +28,9 @@ function page() {
   const port = 5000;
   const socket = io(`${host}:${port}`);
   const [answer, setAnswer] = useState("");
-  const [myAnswer, setMyAnswer] = useState<number>();
+  const [myAnswer, setMyAnswer] = useState<string>();
   const [data, setData] = useState<DataType[]>([]);
+  const [options, setOptions] = useState<OptionsType[]>([]);
 
   socket.on("connection", () => console.log("connect"));
   socket.on("receiveAnswer", (ans) => {
@@ -30,7 +38,7 @@ function page() {
     setAnswer(ans.answer);
   });
   // send message to server
-  function submitAnswer(ans: number) {
+  function submitAnswer(ans: string) {
     socket.emit("submitAnswer", ans);
     setMyAnswer(ans);
   }
@@ -38,34 +46,45 @@ function page() {
     const { data, error } = await supabase.from("quizzes").select();
     if (data === null) return;
     setData(data);
-    console.log(data[0]);
+    console.log(data[7]);
+  };
+  const fetchEq = async () => {
+    let { data } = await supabase
+      .from("options")
+      .select("*")
+      .eq("quiz_id", "8");
+    return data as OptionsType[];
   };
   useEffect(() => {
     fetch();
+    (async () => {
+      setOptions(await fetchEq());
+    })();
   }, []);
 
   return (
     <div>
-      <div>{data[0]?.id}</div>
-      {/* TODO クリックすると管理者に選択した答えが送信される */}
-      <button className="btn" onClick={() => submitAnswer(1)}>
-        1
-      </button>
-      <button className="btn" onClick={() => submitAnswer(2)}>
-        2
-      </button>
-      <button className="btn" onClick={() => submitAnswer(3)}>
-        3
-      </button>
-      <button className="btn" onClick={() => submitAnswer(4)}>
-        4
-      </button>
       <div>
-        <h3 className="text-white">自分の答え：</h3>
-        <h3 className="text-white">{myAnswer}</h3>
-        <h3 className="text-white">正解：</h3>
-        <h3 className="text-white">{answer}</h3>
+        {options.map((option) => {
+          {
+            /* TODO クリックすると管理者に選択した答えが送信される */
+          }
+          return (
+            <div key={option.id}>
+              <button
+                className="btn"
+                onClick={() => submitAnswer(option.option_text)}
+              >
+                {option.option_text}
+              </button>
+            </div>
+          );
+        })}
       </div>
+      <h3 className="text-white">自分の答え：</h3>
+      <h3 className="text-white">{myAnswer}</h3>
+      <h3 className="text-white">正解：</h3>
+      <h3 className="text-white">{answer}</h3>
     </div>
   );
 }
