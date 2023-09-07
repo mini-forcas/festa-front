8511de249c54b7ae3fe5ede2aa223bffa52e9f59
mixnuts <french0719@gmail.com>
2023-09-07 18:55:58 +0900

    add ans

--- app/festa/page.tsx
+++ app/festa/page.tsx
@@ -3,55 +3,59 @@ import React, { useEffect, useState } from "react";
 import { socket, supabase } from "@/constants/const";
 import { QuizChoiceType, QuizType } from "@/types/Quiz";
 
+type AnsType = {
+  answer: string | number;
+};
 function page() {
-  const [data, setData] = useState<QuizType[]>([]);
-  const [options, setOptions] = useState<QuizChoiceType[]>([]);
-  const [answer, setAnswer] = useState<number>();
+  const [quiz, setQuiz] = useState<QuizType[]>([]);
+  const [choices, setChoices] = useState<QuizChoiceType[]>([]);
+  const [correctAnswer, setCorrectAnswer] = useState<string | number>();
   const [myAnswer, setMyAnswer] = useState<string>();
 
   socket.on("connection", () => console.log("connect"));
-  socket.on("receiveAnswer", () => {
-    setAnswer(data[0].correct_option_id);
+  socket.on("receiveAnswer", (ans: AnsType) => {
+    setCorrectAnswer(ans.answer);
   });
   // send message to server
   function submitAnswer(ans: string) {
     socket.emit("submitAnswer", ans);
     setMyAnswer(ans);
   }
-  const fetch = async () => {
+  const fetchAllQuizzes = async () => {
     const { data, error } = await supabase.from("quizzes").select();
     if (data === null) return;
-    setData(data);
-    console.log(data[7]);
+    setQuiz(data);
+    console.log(`QUIZ→${data[0].correct_option_id}`);
   };
-  const fetchEq = async () => {
+  const fetchChoiceByQuiz = async () => {
     let { data } = await supabase
       .from("options")
       .select("*")
+      // TODO
       .eq("quiz_id", "8");
     return data as QuizChoiceType[];
   };
   useEffect(() => {
-    fetch();
+    fetchAllQuizzes();
     (async () => {
-      setOptions(await fetchEq());
+      setChoices(await fetchChoiceByQuiz());
     })();
   }, []);
 
   return (
     <div>
       <div>
-        {options.map((option) => {
+        {choices.map((choice) => {
           {
             /* TODO クリックすると管理者に選択した答えが送信される */
           }
           return (
-            <div key={option.id}>
+            <div key={choice.id}>
               <button
                 className="btn"
-                onClick={() => submitAnswer(option.option_text)}
+                onClick={() => submitAnswer(choice.option_text)}
               >
-                {option.option_text}
+                {choice.option_text}
               </button>
             </div>
           );
@@ -60,7 +64,7 @@ function page() {
       <h3 className="text-white">自分の答え：</h3>
       <h3 className="text-white">{myAnswer}</h3>
       <h3 className="text-white">正解：</h3>
-      <h3 className="text-white">{answer}</h3>
+      <h3 className="text-white">{correctAnswer}</h3>
     </div>
   );
 }
